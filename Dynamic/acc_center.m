37ee006be7f41a660672790d3236a8a8a8a76d15
Cyanlin0104 <37583863+Cyanlin0104@users.noreply.github.com>
2018-11-30 16:46:10 +0800

    Delete acc_center.m

--- Dynamic/acc_center.m
+++
@@ -1,3 +0,0 @@
-function acceleration = acc_center(previous_acc,previous_ang_vel,previous_angular_acc,Pc)
-acceleration=previous_acc+cross(previous_angular_acc,Pc)+cross(previous_ang_vel,cross(previous_ang_vel,Pc));
-end
