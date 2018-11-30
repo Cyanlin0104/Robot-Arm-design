0946a6ef3a9005a5023b93318d8535cea689d871
Cyanlin0104 <37583863+Cyanlin0104@users.noreply.github.com>
2018-11-30 16:46:18 +0800

    Delete acc_pris.m

--- Dynamic/acc_pris.m
+++
@@ -1,3 +0,0 @@
-function acceleration = acc_pris(previous_acc,rotation_matrix,w,w_dot,d_dot,d_ddot,P,w_current)
-acceleration=previous_acc+cross(w_dot,P)+cross(w,cross(w,P))+2*cross(w_current,rotation_matrix'*[0;0;d_dot])+rotation_matrix'*[0;0;d_ddot]
-end
